a7c5b655653ce60e8b43ac3979da0e85ea0b6fa1
ftsan <ftsan12345@gmail.com>
2017-01-08 21:04:30 +0900

    Initial commit

---
+++ src/main/java/com/syu/ho/SyuHoApiApplication.java
@@ -0,0 +1,12 @@
+package com.syu.ho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyuHoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyuHoApiApplication.class, args);
+	}
+}
